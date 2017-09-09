593b1e02dea72f4bb933aa80c2571328ed6bd77d
Foster <brownfosman5000@gmail.com>
2017-09-08 23:10:39 -0400

    Update

--- manager/forms.py
+++ manager/forms.py
@@ -1,1 +1,12 @@
-from django.forms import DateTimeField,CharField,
+from django import forms
+from django.forms import DateTimeField,CharField
+
+
+#Form for ShowTracker
+class ShowTrackerForm(forms.Form):
+	datetime = forms.DateTimeField()
+	venue = forms.CharField(max_length=50)
+	
+	
+		
+
--- manager/views.py
+++ manager/views.py
@@ -2,10 +2,18 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
+import forms
 # Create your views here.
 
 def home(request):
 	return render(request,"manager/home.html")
 
 def addBand(request):
-	return render(request,"manager/addband.html")
+	#if request.method == "POST":
+	
+	showform = forms.ShowTrackerForm(request.POST)
+		
+		#if showform.isValid():
+	        	#HttpResponseRedirect("infostored")		
+	
+	return render(request,"manager/addband.html" ,{"form" : showform})
